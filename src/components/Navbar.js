e07e945899b44d1f01b14e44edfb7b70dc04fb00
GyanendraMohan <gmpatel152@gmail.com>
2023-02-03 01:34:28 +0530

    Added toggle in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/Navbar";
 import Logo from "./Logo";
 import { FaCaretDown, FaAlignLeft, FaUserCircle } from "react-icons/fa";
+import { toggleSidebar } from "../features/user/userSlice";
 
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
+
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar);
+  };
+
   return (
     <Wrapper>
       <div className="nav-center">
-        <button
-          type="button"
-          className="toggle-btn"
-          onClick={() => console.log("toggle sidebar")}
-        >
+        <button type="button" className="toggle-btn" onClick={() => toggle}>
           <FaAlignLeft />
         </button>
         <div>
@@ -25,13 +29,13 @@ const Navbar = () => {
           <button
             type="button"
             className="btn"
-            onClick={() => console.log("toggle dropdown logout")}
+            onClick={() => setShowLogout(!showLogout)}
           >
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
-          <div className="dropdown show-dropdown">
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
             <button
               type="button"
               className="dropdown-btn"
