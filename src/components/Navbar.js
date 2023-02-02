4f70f48c1c82a37942e8b90f5047b47c22c2a9b5
GyanendraMohan <gmpatel152@gmail.com>
2023-02-02 23:21:07 +0530

    added react icons and done with Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/Navbar";
+import Logo from "./Logo";
+import { FaCaretDown, FaAlignLeft, FaUserCircle } from "react-icons/fa";
+
+const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle dropdown logout")}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
